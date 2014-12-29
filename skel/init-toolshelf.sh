4547db3250c65979a2c89aacf3b9eea799a30e92
Chris Pressey <cpressey@catseye.tc>
2014-12-29 20:03:31 +0000

    In theory, there is no reason for this not to work. Untested yet!

--- skel/init-toolshelf.sh
+++ skel/init-toolshelf.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-TOOLSHELF_VERSION=0.1-2014.0930
+PYTHON_VERSION=2.7.8
+TOOLSHELF_VERSION=0.1-2014.1229
 
 cd /home/user
 mkdir -p toolshelf
@@ -13,22 +14,18 @@ if [ ! -d toolshelf/.toolshelf ]; then
 fi
 
 cd toolshelf/localhost/distfile
-if [ ! -e /home/user/toolshelf/localhost/distfile/Python-2.7.6/bin/python ]; then
+if [ ! -e /home/user/toolshelf/localhost/distfile/Python-$PYTHON_VERSION/bin/python ]; then
   echo "About to extract and build Python.  This may take a while."
   echo "Press Enter to continue."
   read LINE
-  tar zxvf /cdrom/Python-2.7.6.tgz || exit 1
-  cd Python-2.7.6
+  tar zxvf /cdrom/Python-$PYTHON_VERSION.tgz || exit 1
+  cd Python-$PYTHON_VERSION
   ( ./configure --prefix=`pwd` && make && make install ) || exit 1
 fi
 
 cd /home/user
 rm -f toolshelf/.bin/python
-ln -s /home/user/toolshelf/localhost/distfile/Python-2.7.6/bin/python toolshelf/.bin/python
+ln -s /home/user/toolshelf/localhost/distfile/Python-$PYTHON_VERSION/bin/python toolshelf/.bin/python
 
 echo >>.profile 'export TOOLSHELF=/home/user/toolshelf && . $TOOLSHELF/.toolshelf/init.sh'
 echo >>.profile 'alias th=toolshelf'
-
-if [ -e /cdrom/local-cookies.catalog -a ! -e toolshelf/.toolshelf/local-cookies.catalog ]; then
-  cp /cdrom/local-cookies.catalog toolshelf/.toolshelf/local-cookies.catalog
-fi
