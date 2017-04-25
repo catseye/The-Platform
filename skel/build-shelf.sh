0e9cecb96632d630bcb0fdbfb52d3d33fca4dd95
Chris Pressey <cpressey@catseye.tc>
2017-04-25 08:55:06 +0100

    Attempt to fix unlinked Python and errant Erlang dirname in build.

--- skel/build-shelf.sh
+++ skel/build-shelf.sh
@@ -21,23 +21,15 @@ chmod 755 chicken-4.9.0.1/build.sh
 cat >otp_src_R16B03-1/build.sh <<'EOF'
 #!/bin/sh
 rm -rf lib/cos* && touch lib/asn1/SKIP lib/eldap/SKIP lib/gs/SKIP lib/hipe/SKIP lib/inets/SKIP lib/jinterface/SKIP lib/megaco/SKIP lib/orber/SKIP lib/odbc/SKIP lib/erlang && ./configure --prefix=`pwd` --disable-threads --disable-hipe --without-ssl && make
+find . -name dirname -exec rm {} \;
 EOF
 chmod 755 otp_src_R16B03-1/build.sh
 
-PROJECTS="
-    Python-2.7.8
-    make-3.82
-    perl-5.20.1
-    hugs98-plus-Sep2006
-    otp_src_R16B03-1
-    chicken-4.9.0.1
-    yasm-1.3.0
-    Ophis-2.1
-"
-
-for p in $PROJECTS; do
+for p in `cat /cdrom/infrastructure.catalog`; do
   echo "*** shelf_build $p ***"
   shelf_build $p
   echo "*** shelf_link $p ***"
   shelf_link $p
 done
+
+ln -s ${SHELF_FARMBASE}/bin/python2.7 ${SHELF_FARMBASE}/bin/python
