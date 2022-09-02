f22a6eaba4d3e41669df7492257b81c9d42888fa
Chris Pressey <cpressey@catseye.tc>
2022-09-02 22:10:06 +0100

    Fix directory names in build-infra.sh script.

--- skel/build-infra.sh
+++ skel/build-infra.sh
@@ -4,19 +4,22 @@
 
 cd /home/user/catseye
 
+PERL=perl-5.37.3
+CHICKEN=chicken-4.13.0
+
 # write out cookies
 
-cat >perl-5.*/build.sh <<'EOF'
+cat >$PERL/build.sh <<'EOF'
 #!/bin/sh
 C_INCLUDE_PATH="" ./Configure -des -Dprefix=`pwd`/install && C_INCLUDE_PATH="" make && make install
 EOF
-chmod 755 perl-5.*/build.sh
+chmod 755 $PERL/build.sh
 
-cat >chicken-4.*/build.sh <<'EOF'
+cat >$CHICKEN/build.sh <<'EOF'
 #!/bin/sh
 mkdir -p install && make PLATFORM=bsd PREFIX=`pwd`/install VARDIR=`pwd`/install install
 EOF
-chmod 755 chicken-4.*/build.sh
+chmod 755 $CHICKEN/build.sh
 
 for line in `cat /cdrom/infrastructure.catalog`; do
   project=`echo $line | awk '{split($0,a,"@"); print a[1]}'`
