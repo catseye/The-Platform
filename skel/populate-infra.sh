58cc26e3d3274fd99d8dcf6546ec8fa55cbd3406
Chris Pressey <cpressey@catseye.tc>
2023-03-10 05:26:13 +0000

    Give directories names that match their distribution names.

--- skel/populate-infra.sh
+++ skel/populate-infra.sh
@@ -7,3 +7,5 @@ mkdir -p catseye
 cd catseye
 
 shelf_populate_from_distfiles /cdrom/ < /cdrom/infrastructure.catalog || exit 1
+mv catseye/Ophis-99f074da278d4ec80689c0e22e20c5552ea12512 catseye/Ophis-2.1-2019.0413
+mv catseye/dcc6502-dcc6502_v2.1 catseye/dcc6502_v2.1
