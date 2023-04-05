aae5e20c6d6510999270b1c2510337467fc59f86
Chris Pressey <cpressey@catseye.tc>
2023-04-05 16:54:29 +0100

    Fix typo in directory name.

--- skel/populate-infra.sh
+++ skel/populate-infra.sh
@@ -8,4 +8,4 @@ cd catseye
 
 shelf_populate_from_distfiles /cdrom/ < /cdrom/infrastructure.catalog || exit 1
 mv Ophis-99f074da278d4ec80689c0e22e20c5552ea12512 Ophis-2.1-2019.0413
-mv dcc6502-680c21299241133449056c2ddfbc0dd087dc3807 dcc6502-v2.1-2018.0902
+mv dcc6502-680c21299241133449056c2ddfbc0dd087dc3807 dcc6502-2.1-2018.0902
