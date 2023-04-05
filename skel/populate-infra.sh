ad7ae3e996e53124f4b9232c5c93bc4b5d09992f
Chris Pressey <cpressey@catseye.tc>
2023-04-05 14:31:08 +0100

    Get the altered package names consistent.

--- skel/populate-infra.sh
+++ skel/populate-infra.sh
@@ -7,5 +7,5 @@ mkdir -p catseye
 cd catseye
 
 shelf_populate_from_distfiles /cdrom/ < /cdrom/infrastructure.catalog || exit 1
-mv catseye/Ophis-99f074da278d4ec80689c0e22e20c5552ea12512 catseye/Ophis-2.1-2019.0413
-mv catseye/dcc6502-dcc6502_v2.1 catseye/dcc6502_v2.1
+mv Ophis-99f074da278d4ec80689c0e22e20c5552ea12512 Ophis-2.1-2019.0413
+mv dcc6502-680c21299241133449056c2ddfbc0dd087dc3807 dcc6502-v2.1-2018.0902
